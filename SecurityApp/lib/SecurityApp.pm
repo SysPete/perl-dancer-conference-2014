abdfcd0602d1a34df891d6778c134f04ac608c23
Peter Mottram <peter@sysnix.com>
2014-08-22 18:06:21 +0200

    add SecurityApp

--- SecurityApp/lib/SecurityApp.pm
+++ SecurityApp/lib/SecurityApp.pm
@@ -1,10 +1,17 @@
 package SecurityApp;
+
 use Dancer ':syntax';
+use Dancer::Plugin::Auth::Extensible;
+use Dancer::Plugin::DBIC;
+use Interchange6::Schema;
 
 our $VERSION = '0.1';
 
+set session => 'DBIC';
+set session_options => { schema => schema, };
+
 get '/' => sub {
-    template 'index';
+    return "session id: " . session->id;
 };
 
 true;
